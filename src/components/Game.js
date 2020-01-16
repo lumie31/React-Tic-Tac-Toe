63437a551ba80ba6ca0c801192e132ac31cd4f9d
Olumide Okedusi <odokedusi@sidmach.com>
2020-01-16 10:25:42 +0100

    Lift state up to Game component + style app with css

--- src/components/Game.js
+++ src/components/Game.js
@@ -65,15 +65,23 @@ class Game extends Component {
       status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
     }
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
+      <>
+        <h3>
+          <ins>TIC-TAC-TOE</ins>
+        </h3>
+        <div className="game">
+          <div className="game-board">
+            <Board
+              squares={current.squares}
+              onClick={i => this.handleClick(i)}
+            />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{moves}</ol>
+          </div>
         </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
-        </div>
-      </div>
+      </>
     );
   }
 }
